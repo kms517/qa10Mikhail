80763ca62a4b3132b30bda38d838a95839768174
k517ms <k517ms@gmail.com>
2017-07-19 12:09:15 +0300

    Create GroupDeletionTest and TestBase

--- selenium-java/src/test/java/GroupDeletionTest.java
+++ selenium-java/src/test/java/GroupDeletionTest.java
@@ -2,47 +2,20 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
-public class GroupDeletionTest extends TestBase{
-
+public class GroupDeletionTest extends TestBase {
 
     @Test
     public void groupDeletionTest() {
-
-        selectGroups();
-        //selectGroups
-        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
-        //clickDeleteButton
-        wd.findElement(By.linkText("group page")).click();
-        //returnToGroupPage
-        wd.findElement(By.linkText("Logout")).click();
-        //logOut
-    }
-
-    private void selectGroups() {
-        wd.findElement(By.linkText("groups")).click();
-        if (!wd.findElement(By.name("selected[]")).isSelected()) {
-            wd.findElement(By.name("selected[]")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/span[3]/input")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/span[3]/input")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/span[5]/input")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/span[5]/input")).click();
-        }
+        goToGroupsPage();
+        selectGroup();
+        deleteGroup();
+        returnGroupsPage();
     }
 
 }
--- selenium-java/src/test/java/TestBase.java
+++ selenium-java/src/test/java/TestBase.java
@@ -7,23 +7,9 @@ import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
-
 public class TestBase {
     FirefoxDriver wd;
 
-
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        openAddressbook();
-        logIn();
-    }
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
@@ -33,11 +19,15 @@ public class TestBase {
         }
     }
 
-    public void openAddressbook() {
-        wd.get("http://localhost/addressbook/");
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        openAddressbook();
+        logIn();
     }
 
-    public void logIn() {
+       protected void logIn() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -47,6 +37,50 @@ public class TestBase {
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
+       public void openAddressbook() {
+        wd.get("http://localhost/addressbook/");
+    }
 
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
 
+    protected void returnToGroupsPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    protected void deleteGroup() {
+        wd.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
+    }
+
+    protected void goToGroupsPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    protected void returnGroupsPage() {
+        returnToGroupsPage();
+    }
+
+    protected void submitGroupsCreation() {
+
+        wd.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm() {
+        wd.findElement(By.name("new")).click();
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys("groupname");
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys("header");
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys("footer");
+    }
 }
